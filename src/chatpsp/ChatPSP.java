16ff7ede3af71dbd13f5f2e8153470de817c1040
RTM841 <rtejmar841@g.educaand.es>
2024-01-22 18:07:46 +0100

    Versión1

---
+++ src/chatpsp/ChatPSP.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chatpsp;
+
+/**
+ *
+ * @author ruben
+ */
+public class ChatPSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
